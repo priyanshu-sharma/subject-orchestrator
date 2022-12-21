919b3a7595294bf914f92267e016fff3c967e7dc
priyanshu-sharma <prynsh1112@gmail.com>
2022-12-20 23:38:44 -0800

    added migration for courses model

--- src/orchestrator_engine/school_domain/models/__init__.py
+++ src/orchestrator_engine/school_domain/models/__init__.py
@@ -1,3 +1,3 @@
-from school_domain.models.courses import Courses
-from school_domain.models.department import Department
 from school_domain.models.schools import Schools
+from school_domain.models.department import Department
+from school_domain.models.courses import Courses
