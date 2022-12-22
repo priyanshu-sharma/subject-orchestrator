bdb11d096d5a22bbc32c69f7c2509c805eb37bed
priyanshu-sharma <prynsh1112@gmail.com>
2022-12-21 19:04:24 -0800

    added course code in courses model

--- src/orchestrator_engine/school_domain/models/courses.py
+++ src/orchestrator_engine/school_domain/models/courses.py
@@ -5,6 +5,7 @@ from school_domain.models import Department
 class Courses(AutoTimestampedModel, UserTrackingModel):
     id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     schedule = models.JSONField(default=dict)
     meta = models.JSONField(default=dict)
@@ -13,16 +14,16 @@ class Courses(AutoTimestampedModel, UserTrackingModel):
     class Meta:
         db_table = 'so_courses'
         app_label = 'school_domain'
-        unique_together = [('name'), ('department')]
+        unique_together = [('name'), ('code'), ('department')]
         indexes = [
-            models.Index(fields=['name', 'department'])
+            models.Index(fields=['name', 'code', 'department'])
         ]
 
     @staticmethod
-    def get_or_create(name, department, schedule, meta, active):
+    def get_or_create(name, code, department, schedule, meta, active):
         try:
-            courses = Courses.objects.get(name=name, department=department, schedule=schedule, meta=meta, active=active)
+            courses = Courses.objects.get(name=name, code=code, department=department, schedule=schedule, meta=meta, active=active)
         except Courses.DoesNotExist:
-            courses = Courses.objects.create(name=name, department=department, schedule=schedule, meta=meta, active=active)
+            courses = Courses.objects.create(name=name, code=code, department=department, schedule=schedule, meta=meta, active=active)
             courses.save()
         return courses
